3750e8a8de6a690e183c62d6d0c32d12c93a105c
Najeeb Rifaat <mohamme89d@gmail.com>
2019-12-26 22:30:35 +0800

    chore(cleanup): update config

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ float alpha = 0.8;
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
